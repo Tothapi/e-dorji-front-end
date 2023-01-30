cdfddfb3a828ea7e16b18e2ce34573b2fa548c0c
riyadh <mrriyadh66@gmail.com>
2023-01-30 23:10:54 +0600

    order table design updated.

--- src/pages/AllOrders.js
+++ src/pages/AllOrders.js
@@ -25,16 +25,38 @@ export default function AllOrders() {
           </div>
         ) : (
           <div>
+            <div className="flex pb-4">
+              <p className="w-[25%] text-center font-extrabold underline">
+                User Mail
+              </p>
+              <p className="w-[25%] font-extrabold text-center underline">
+                List of Products
+              </p>
+              <p className="w-[25%] font-extrabold text-center underline">
+                Address
+              </p>
+              <p className="w-[25%] font-extrabold text-center underline">
+                Price
+              </p>
+            </div>
             {orders?.map((order) => {
               return (
                 <div
                   className="flex items-center justify-between w-full overflow-x-hidden"
                   key={order?._id}
                 >
-                  <p>{order?.userMail || "no User mail found"}</p>
-                  <p>{order?.products || "no Products found"}</p>
-                  <p>{order?.address || "no Address found"}</p>
-                  <p>{order?.totalPrice || "no Price found"}</p>
+                  <p className="w-[25%] text-center">
+                    {order?.userMail || "no User mail found"}
+                  </p>
+                  <p className="w-[25%] text-center">
+                    {order?.products || "no Products found"}
+                  </p>
+                  <p className="w-[25%] text-center">
+                    {order?.address || "no Address found"}
+                  </p>
+                  <p className="w-[25%] text-center">
+                    {order?.totalPrice || "no Price found"}
+                  </p>
                 </div>
               );
             })}
